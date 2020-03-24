f4a1896d62dadfa799951928dd72d9f10ff1b90f
Suresh Chakrapani <61193970+suresh-chakrapani@users.noreply.github.com>
2020-03-24 17:21:23 +0530

    Added missing imports

--- Car/car_example.py
+++ Car/car_example.py
@@ -1,6 +1,8 @@
 # Load Libraries
 from pandas import read_csv
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
